51109f3c50ebf9a2404c52de2b1336484b04a020
Laiun Miguel <LaiunMiguel@hotmail.com>
2024-09-27 20:35:42 -0300

    Grandes Avances

--- js/navbar.js
+++ js/navbar.js
@@ -5,12 +5,21 @@ document.addEventListener('DOMContentLoaded', function() {
         <a href="../"><img src="../images/logo.jpg" alt="Logo" class="logo-img"></a> <!-- Ruta relativa desde un subdirectorio -->
       </div>
       <ul class="nav-links">
+        <div>
         <li><a href="../">Inicio</a></li>
+        </div>
+        <div>
         <li><a href="../enlaces/productos.html">Productos</a></li>
+        </div>
+        <div>
         <li><a href="../enlaces/sobreMi.html">Sobre mi</a></li>
+        </div>
+        <div>
         <li><a href="../enlaces/contacto.html">Contactame</a></li>
+        </div>
+        <div>
         <li><a href="https://www.instagram.com/dtejidosetrata/?hl=es-es" target="_blank">Instagram</a></li>
-        <li><a href="https://www.facebook.com/tu_perfil" target="_blank">Facebook</a></li>
+        </div>
       </ul>
     </nav>
   `;
